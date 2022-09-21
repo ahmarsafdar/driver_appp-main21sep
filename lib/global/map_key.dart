917ec977ab1e587a477bc7f755c1988b942b7199
ahmarsafdar <sme16648@gmail.com>
2022-09-21 22:12:16 +0500

    Initial commit

---
+++ lib/global/map_key.dart
@@ -0,0 +1,3 @@
+//String mapKey = "AIzaSyDo-oNgEbbqe1frJZhwNVRRoOFqNiUQ_-Y";
+//AIzaSyDmDzTn_ND9UD-iedcW75Fs9PwSqlixXIw
+String mapKey = "AIzaSyDmDzTn_ND9UD-iedcW75Fs9PwSqlixXIw";
---
+++ lib/infoHandler/app_info.dart
@@ -0,0 +1,41 @@
+import 'package:driver_app/models/directions.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../models/trips_history_model.dart';
+
+class AppInfo extends ChangeNotifier {
+  Directions? userPickUpLocation, userDropOffLocation;
+  int countTotalTrips = 0;
+  List<String> historyTripsKeysList = [];
+  String driverTotalEarnings = "0";
+  List<TripsHistoryModel> allTripsHistoryInformationList = [];
+
+  void updatePickUpLocationAddress(Directions userPickUpAddress) {
+    userPickUpLocation = userPickUpAddress;
+    notifyListeners();
+  }
+
+  void updateDropOffLocationAddress(Directions dropOffAddress) {
+    userDropOffLocation = dropOffAddress;
+    notifyListeners();
+  }
+
+  updateOverAllTripsCounter(int overAllTripsCounter) {
+    countTotalTrips = overAllTripsCounter;
+    notifyListeners();
+  }
+
+  updateOverAllTripsKeys(List<String> tripsKeysList) {
+    historyTripsKeysList = tripsKeysList;
+    notifyListeners();
+  }
+
+  updateOverAllTripsHistoryInformation(TripsHistoryModel eachTripHistory) {
+    allTripsHistoryInformationList.add(eachTripHistory);
+    notifyListeners();
+  }
+
+  updateDriverTotalEarnings(String driverning) {
+    driverTotalEarnings = driverning;
+  }
+}
